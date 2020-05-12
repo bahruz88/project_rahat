61e5b6a65c9277a35b19408b5a641020da9c107d
Bahruz Qasimov <Bahruz.Qasimov@pashabank.az>
2020-05-12 16:46:28 +0400

    Update users.php
    Test  Yoxlama

--- users.php
+++ users.php
@@ -13,7 +13,7 @@ $result_roles_view = $db->query($sql_roles);
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>RahatHR</title>
-  <!-- Tell the browser to be responsive to screen width -->
+  <!-- Test  yoxlama Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   
    
