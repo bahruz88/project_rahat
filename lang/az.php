cad10baba9fb6c039f1cfb1d421308c9ea8d962b
Bahruz Qasimov <Bahruz.Qasimov@pashabank.az>
2020-05-13 09:30:14 +0400

    Update az.php
    Family info  ile bagli
    family information ve  contract_number elave edildi

--- lang/az.php
+++ lang/az.php
@@ -112,10 +112,12 @@ $dil = array(
 	"skill_edit_title"   =>"Bacarıq məlumatlarının redaktəsi",
 	"lang_view_title"   =>"Dil biliklərinə baxış",
 	"lang_edit_title"   =>"Dil biliklərinin redaktəsi",
+	"family_information"   =>"Ailə məlumatları",
 	"family_member_name"   =>"Ailə  üzvünün  adı",
 	"family_member_type"   =>"Ailə  üzvünün  tipi",
 	"family_member_phone"   =>"Telefon  nömrəsi",
 	"family_member_adress"   =>"Adresi",
+	"contact_number"   =>"Əlaqə nömrəsi",
 	"operation"   =>"Əməliyyat"
 );
 ?>
