bd30c8554cdf8c62bcd97ebf104926b3f4a44d90
Bahruz Qasimov <Bahruz.Qasimov@pashabank.az>
2020-06-15 16:32:30 +0400

    Update employee.js

--- js/employee.js
+++ js/employee.js
@@ -3609,7 +3609,7 @@ $('#drivinglic_issue_date').datetimepicker({ format: 'DD/MM/YYYY'  });
  	$("#view_wp_permit_date").datetimepicker({ format: 'DD/MM/YYYY'  });
 	$("#view_wp_valid_date").datetimepicker({ format: 'DD/MM/YYYY'  });
 
-$("#trp_permit_date").datetimepicker({ format: 'DD/MM/YYYY'  });
+    $("#trp_permit_date").datetimepicker({ format: 'DD/MM/YYYY'  });
 	$("#trp_valid_date").datetimepicker({ format: 'DD/MM/YYYY'  });
  	$("#prp_permit_date").datetimepicker({ format: 'DD/MM/YYYY'  });
 	$("#prp_valid_date").datetimepicker({ format: 'DD/MM/YYYY'  });
