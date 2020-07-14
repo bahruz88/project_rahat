27f7240854cb3885347273218739a7c939e85d65
Mayya-S <65227777+Mayya-S@users.noreply.github.com>
2020-07-14 09:35:12 +0400

    UPDATE struktur2/index.php

--- struktur2/index.php
+++ struktur2/index.php
@@ -33,7 +33,7 @@ $st_type =implode(",", $st_type);
 <input type="hidden" id="create_date" name="create_date" value="<?php echo $create_date; ?>">
 <input type="hidden" id="st_type" name="st_type" value="<?php echo $st_type; ?>">
 <input type="hidden" id="user_id_edit" name="user_id_edit" value="">
-<input type="text" id="user_name" name="user_name" value="">
+<input type="hidden" id="user_name" name="user_name" value="">
 <!DOCTYPE html>
 <html>
 	<head>
@@ -96,77 +96,72 @@ $st_type =implode(",", $st_type);
 	</head>
 
 	<body class="example">
-		<h1>
-			Example: tree grid with keyboard navigation, DnD, and editing
-			capabilites
-		</h1>
-		<div class="description">
-			Bringing it all together: this sample combines different extensions
-			and custom events to implement an editable tree grid:
-			<ul>
-				<li>'ext-dnd' to re-order nodes using drag-and-drop.</li>
-				<li>
-					'ext-table' + 'ext-gridnav' to implement a tree grid.<br />
-					Try <kbd>UP / DOWN / LEFT / RIGHT</kbd>, <kbd>TAB</kbd>,
-					<kbd>Shift+TAB</kbd>
-					to navigate between grid cells. Note that embedded input
-					controls remain functional.
-				</li>
-				<li>
-					'ext-edit': inline editing.<br />
-					Try <kbd>F2</kbd> to rename a node.<br />
-					<kbd>Ctrl+N</kbd>, <kbd>Ctrl+Shift+N</kbd> to add nodes
-					(Quick-enter: add new nodes until [enter] is hit on an empty
-					title).
-				</li>
-				<li>
-					Extended keyboard shortcuts:<br />
-					<kbd>Ctrl+C</kbd>, <kbd>Ctrl+X</kbd>, <kbd>Ctrl+V</kbd> for
-					copy/paste,<br />
-					<kbd>Ctrl+UP</kbd>, <kbd>Ctrl+DOWN</kbd>,
-					<kbd>Ctrl+LEFT</kbd>, <kbd>Ctrl+RIGHT</kbd>
-          to move nodes around and change indentation.<br>
-          (On macOS, add <kbd>Shift</kbd> to the keystrokes.)
-				</li>
-				<li>
-					3rd-party
-					<a href="https://github.com/mar10/jquery-ui-contextmenu"
-						>contextmenu</a
-					>
-					for additional edit commands
-				</li>
-			</ul>
-		</div>
-		<div>
-			<label for="skinswitcher">Skin:</label>
-			<select id="skinswitcher"></select>
-		</div>
-
-		<h1>Table Tree</h1>
-		<div>
-			<label>Fake input:<input id="input1"/></label>
-		</div>
+    <h2>STRUKTUR</h2>
+<!--		<h1>-->
+<!--			Example: tree grid with keyboard navigation, DnD, and editing-->
+<!--			capabilites-->
+<!--		</h1>-->
+<!--		<div class="description">-->
+<!--			Bringing it all together: this sample combines different extensions-->
+<!--			and custom events to implement an editable tree grid:-->
+<!--			<ul>-->
+<!--				<li>'ext-dnd' to re-order nodes using drag-and-drop.</li>-->
+<!--				<li>-->
+<!--					'ext-table' + 'ext-gridnav' to implement a tree grid.<br />-->
+<!--					Try <kbd>UP / DOWN / LEFT / RIGHT</kbd>, <kbd>TAB</kbd>,-->
+<!--					<kbd>Shift+TAB</kbd>-->
+<!--					to navigate between grid cells. Note that embedded input-->
+<!--					controls remain functional.-->
+<!--				</li>-->
+<!--				<li>-->
+<!--					'ext-edit': inline editing.<br />-->
+<!--					Try <kbd>F2</kbd> to rename a node.<br />-->
+<!--					<kbd>Ctrl+N</kbd>, <kbd>Ctrl+Shift+N</kbd> to add nodes-->
+<!--					(Quick-enter: add new nodes until [enter] is hit on an empty-->
+<!--					title).-->
+<!--				</li>-->
+<!--				<li>-->
+<!--					Extended keyboard shortcuts:<br />-->
+<!--					<kbd>Ctrl+C</kbd>, <kbd>Ctrl+X</kbd>, <kbd>Ctrl+V</kbd> for-->
+<!--					copy/paste,<br />-->
+<!--					<kbd>Ctrl+UP</kbd>, <kbd>Ctrl+DOWN</kbd>,-->
+<!--					<kbd>Ctrl+LEFT</kbd>, <kbd>Ctrl+RIGHT</kbd>-->
+<!--          to move nodes around and change indentation.<br>-->
+<!--          (On macOS, add <kbd>Shift</kbd> to the keystrokes.)-->
+<!--				</li>-->
+<!--				<li>-->
+<!--					3rd-party-->
+<!--					<a href="https://github.com/mar10/jquery-ui-contextmenu"-->
+<!--						>contextmenu</a-->
+<!--					>-->
+<!--					for additional edit commands-->
+<!--				</li>-->
+<!--			</ul>-->
+<!--		</div>-->
+<!--		<div>-->
+<!--			<label for="skinswitcher">Skin:</label>-->
+<!--			<select id="skinswitcher"></select>-->
+<!--		</div>-->
+<!---->
+<!--		<h1>Table Tree</h1>-->
+<!--		<div>-->
+<!--			<label>Fake input:<input id="input1"/></label>-->
+<!--		</div>-->
 		<table id="tree">
 			<colgroup>
 				<col width="30px" />
 				<col width="50px" />
 				<col width="350px" />
-				<col width="50px" />
-				<col width="50px" />
-				<col width="30px" />
-				<col width="30px" />
-				<col width="50px" />
+				<col width="150px" />
+				<col width="150px" />
 			</colgroup>
 			<thead>
 				<tr>
 					<th></th>
 					<th>#</th>
 					<th></th>
-					<th>Ed1</th>
-					<th>Ed2</th>
-					<th>Rb1</th>
-					<th>Rb2</th>
-					<th>Cb</th>
+					<th> Tip </th>
+					<th>İl</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -179,32 +174,32 @@ $st_type =implode(",", $st_type);
 					<td></td>
 <!--					<td><input name="input1" type="input" /></td>-->
 <!--					<td><input name="input2" type="input" /></td>-->
-					<td class="alignCenter">
-						<input name="cb1" type="checkbox" />
-					</td>
-					<td class="alignCenter">
-						<input name="cb2" type="checkbox" />
-					</td>
-					<td>
-						<select name="sel1" id="">
-							<option value="a">A</option>
-							<option value="b">B</option>
-						</select>
-					</td>
+<!--					<td class="alignCenter">-->
+<!--						<input name="cb1" type="checkbox" />-->
+<!--					</td>-->
+<!--					<td class="alignCenter">-->
+<!--						<input name="cb2" type="checkbox" />-->
+<!--					</td>-->
+<!--					<td>-->
+<!--						<select name="sel1" id="">-->
+<!--							<option value="a">A</option>-->
+<!--							<option value="b">B</option>-->
+<!--						</select>-->
+<!--					</td>-->
 				</tr>
 			</tbody>
 		</table>
 
 		<!-- Start_Exclude: This block is not part of the sample code -->
-		<hr />
-		<p class="sample-links  no_code">
-			<a class="hideInsideFS" href="https://github.com/mar10/fancytree"
-				>jquery.fancytree.js project home</a
-			>
-			<a class="hideOutsideFS" href="#">Link to this page</a>
-			<a class="hideInsideFS" href="index.html">Example Browser</a>
-			<a href="#" id="codeExample">View source code</a>
-		</p>
+<!--		<hr />-->
+<!--		<p class="sample-links  no_code">-->
+<!--			<a class="hideInsideFS" href="https://github.com/mar10/fancytree"-->
+<!--				>jquery.fancytree.js project home</a-->
+<!--			>-->
+<!--			<a class="hideOutsideFS" href="#">Link to this page</a>-->
+<!--			<a class="hideInsideFS" href="index.html">Example Browser</a>-->
+<!--			<a href="#" id="codeExample">View source code</a>-->
+<!--		</p>-->
 		<pre id="sourceCode" class="prettyprint" style="display:none"></pre>
 		<!-- End_Exclude -->
 	</body>
@@ -250,6 +245,7 @@ $st_type =implode(",", $st_type);
                 type: "POST",
                 data: {parent:parentId},
                 success: function (data) {
+                    console.log('data ss=',data)
 
                     var jsonARR=jQuery.parseJSON(data);
 
@@ -257,14 +253,14 @@ $st_type =implode(",", $st_type);
                     childrenArray=[];
                     $.each(jsonARR, function(k,v){
 
-                        childrenArray.push({"id":v[0],"title":v[1],"pId":v[2],"st_type":v[3],"year":v[4],"type":v[4],"author":v[2],"qty":332,"price":224});
-                        console.log('childrenArray=',childrenArray)
+                        childrenArray.push({"id":v[0],"title":v[1],"pId":v[2],"st_type":v[4],"year":v[3],"type":v[4],"author":v[2],"qty":332,"price":224});
+                        // console.log('childrenArray=',childrenArray)
 
                     })
                     zNodeArray.push({"id":userIdArray[j],"title":userArray[j],"pId":parentArray[j],"st_type":stTypeArray[j],"year":createDateArray[j], "expanded":true,"folder": true,"children":childrenArray});
                     //zNodeArray+=[{"title":userArray[j], "expanded":true,"folder": true,"children":childrenArray}]
 
-                    console.log('zNodeArray=',zNodeArray)
+                    // console.log('zNodeArray=',zNodeArray)
                     pushOldu(zNodeArray);
                 },
             });
@@ -274,44 +270,44 @@ $st_type =implode(",", $st_type);
 
         function pushOldu(zNodeArray) {
             pushArray = zNodeArray
-            console.log('pushArray=', pushArray)
+            // console.log('pushArray=', pushArray)
 
-            var SOURCE = [
-                    {"title": "Books", "expanded": true, "folder": true, "children": [
-                            {"title": "Art of War", "type": "book", "author": "Sun Tzu", "year": -500, "qty": 21, "price": 5.95},
-                            {"title": "The Hobbit", "type": "book", "author": "J.R.R. Tolkien", "year": 1937, "qty": 32, "price": 8.97},
-                            {"title": "The Little Prince", "type": "book", "author": "Antoine de Saint-Exupery", "year": 1943, "qty": 2946, "price": 6.82},
-                            {"title": "Don Quixote", "type": "book", "author": "Miguel de Cervantes", "year": 1615, "qty": 932, "price": 15.99}
-                        ]},
-                    {"title": "Music", "folder": true, "children": [
-                            {"title": "Nevermind", "type": "music", "author": "Nirvana", "year": 1991, "qty": 916, "price": 15.95},
-                            {"title": "Autobahn", "type": "music", "author": "Kraftwerk", "year": 1974, "qty": 2261, "price": 23.98},
-                            {"title": "Kind of Blue", "type": "music", "author": "Miles Davis", "year": 1959, "qty": 9735, "price": 21.90},
-                            {"title": "Back in Black", "type": "music", "author": "AC/DC", "year": 1980, "qty": 3895, "price": 17.99},
-                            {"title": "The Dark Side of the Moon", "type": "music", "author": "Pink Floyd", "year": 1973, "qty": 263, "price": 17.99},
-                            {"title": "Sgt. Pepper's Lonely Hearts Club Band", "type": "music", "author": "The Beatles", "year": 1967, "qty": 521, "price": 13.98}
-                        ]},
-                    {"title": "Electronics & Computers", "expanded": true, "folder": true, "children": [
-                            {"title": "Cell Phones", "folder": true, "children": [
-                                    {"title": "Moto G", "type": "phone", "author": "Motorola", "year": 2014, "qty": 332, "price": 224.99},
-                                    {"title": "Galaxy S8", "type": "phone", "author": "Samsung", "year": 2016, "qty": 952, "price": 509.99},
-                                    {"title": "iPhone SE", "type": "phone", "author": "Apple", "year": 2016, "qty": 444, "price": 282.75},
-                                    {"title": "G6", "type": "phone", "author": "LG", "year": 2017, "qty": 951, "price": 309.99},
-                                    {"title": "Lumia", "type": "phone", "author": "Microsoft", "year": 2014, "qty": 32, "price": 205.95},
-                                    {"title": "Xperia", "type": "phone", "author": "Sony", "year": 2014, "qty": 77, "price": 195.95},
-                                    {"title": "3210", "type": "phone", "author": "Nokia", "year": 1999, "qty": 3, "price": 85.99}
-                                ]},
-                            {"title": "Computers", "folder": true, "children": [
-                                    {"title": "ThinkPad", "type": "computer", "author": "IBM", "year": 1992, "qty": 16, "price": 749.90},
-                                    {"title": "C64", "type": "computer", "author": "Commodore", "year": 1982, "qty": 83, "price": 595.00},
-                                    {"title": "MacBook Pro", "type": "computer", "author": "Apple", "year": 2006, "qty": 482, "price": 1949.95},
-                                    {"title": "Sinclair ZX Spectrum", "type": "computer", "author": "Sinclair Research", "year": 1982, "qty": 1, "price": 529},
-                                    {"title": "Apple II", "type": "computer", "author": "Apple", "year": 1977, "qty": 17, "price": 1298},
-                                    {"title": "PC AT", "type": "computer", "author": "IBM", "year": 1984, "qty": 3, "price": 1235.00}
-                                ]}
-                        ]},
+            // var SOURCE = [
+            //         {"title": "Books", "expanded": true, "folder": true, "children": [
+            //                 {"title": "Art of War", "type": "book", "author": "Sun Tzu", "year": -500, "qty": 21, "price": 5.95},
+            //                 {"title": "The Hobbit", "type": "book", "author": "J.R.R. Tolkien", "year": 1937, "qty": 32, "price": 8.97},
+            //                 {"title": "The Little Prince", "type": "book", "author": "Antoine de Saint-Exupery", "year": 1943, "qty": 2946, "price": 6.82},
+            //                 {"title": "Don Quixote", "type": "book", "author": "Miguel de Cervantes", "year": 1615, "qty": 932, "price": 15.99}
+            //             ]},
+            //         {"title": "Music", "folder": true, "children": [
+            //                 {"title": "Nevermind", "type": "music", "author": "Nirvana", "year": 1991, "qty": 916, "price": 15.95},
+            //                 {"title": "Autobahn", "type": "music", "author": "Kraftwerk", "year": 1974, "qty": 2261, "price": 23.98},
+            //                 {"title": "Kind of Blue", "type": "music", "author": "Miles Davis", "year": 1959, "qty": 9735, "price": 21.90},
+            //                 {"title": "Back in Black", "type": "music", "author": "AC/DC", "year": 1980, "qty": 3895, "price": 17.99},
+            //                 {"title": "The Dark Side of the Moon", "type": "music", "author": "Pink Floyd", "year": 1973, "qty": 263, "price": 17.99},
+            //                 {"title": "Sgt. Pepper's Lonely Hearts Club Band", "type": "music", "author": "The Beatles", "year": 1967, "qty": 521, "price": 13.98}
+            //             ]},
+            //         {"title": "Electronics & Computers", "expanded": true, "folder": true, "children": [
+            //                 {"title": "Cell Phones", "folder": true, "children": [
+            //                         {"title": "Moto G", "type": "phone", "author": "Motorola", "year": 2014, "qty": 332, "price": 224.99},
+            //                         {"title": "Galaxy S8", "type": "phone", "author": "Samsung", "year": 2016, "qty": 952, "price": 509.99},
+            //                         {"title": "iPhone SE", "type": "phone", "author": "Apple", "year": 2016, "qty": 444, "price": 282.75},
+            //                         {"title": "G6", "type": "phone", "author": "LG", "year": 2017, "qty": 951, "price": 309.99},
+            //                         {"title": "Lumia", "type": "phone", "author": "Microsoft", "year": 2014, "qty": 32, "price": 205.95},
+            //                         {"title": "Xperia", "type": "phone", "author": "Sony", "year": 2014, "qty": 77, "price": 195.95},
+            //                         {"title": "3210", "type": "phone", "author": "Nokia", "year": 1999, "qty": 3, "price": 85.99}
+            //                     ]},
+            //                 {"title": "Computers", "folder": true, "children": [
+            //                         {"title": "ThinkPad", "type": "computer", "author": "IBM", "year": 1992, "qty": 16, "price": 749.90},
+            //                         {"title": "C64", "type": "computer", "author": "Commodore", "year": 1982, "qty": 83, "price": 595.00},
+            //                         {"title": "MacBook Pro", "type": "computer", "author": "Apple", "year": 2006, "qty": 482, "price": 1949.95},
+            //                         {"title": "Sinclair ZX Spectrum", "type": "computer", "author": "Sinclair Research", "year": 1982, "qty": 1, "price": 529},
+            //                         {"title": "Apple II", "type": "computer", "author": "Apple", "year": 1977, "qty": 17, "price": 1298},
+            //                         {"title": "PC AT", "type": "computer", "author": "IBM", "year": 1984, "qty": 3, "price": 1235.00}
+            //                     ]}
+            //             ]},
                     // {"title": "More...", "folder": true, "lazy": true}
-                ];
+                // ];
             // console.log('SOURCE=',SOURCE)
 
 
@@ -370,7 +366,7 @@ $st_type =implode(",", $st_type);
                     createNode: function(event, data) {
                         var node = data.node,
                             $tdList = $(node.tr).find(">td");
-                        console.log('createNode node=',node)
+                        // console.log('createNode node=',node)
 
                         // Span the remaining columns if it's a folder.
                         // We can do this in createNode instead of renderColumns, because
@@ -388,7 +384,8 @@ $st_type =implode(",", $st_type);
 
                         var node = data.node,
                             $tdList = $(node.tr).find(">td");
-                        console.log('renderColumns node=',node)
+                        // console.log('renderColumns node=',node)
+                        // console.log('renderColumns node.year=',node.data.year)
                         // (Index #0 is rendered by fancytree by adding the checkbox)
                         // Set column #1 info from node data:
                         $tdList.eq(1).text(node.getIndexHier());
@@ -398,12 +395,12 @@ $st_type =implode(",", $st_type);
                         // Set column #3 info from node data:
                         $tdList
                             .eq(3)
-                            .text('33');
+                            .text(node.data.st_type);
                             // .find("input")
                             // .val(node.key);
                         $tdList
                             .eq(4)
-                            .text('44');
+                            .text(node.data.year);
                             // .find("input")
                             // .val(node.data.foo);
                         // $tdList
