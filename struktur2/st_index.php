09a2e3974b7b1e844888cc5f2b00a7cd8677b04b
Mayya-S <65227777+Mayya-S@users.noreply.github.com>
2020-07-15 14:49:51 +0400

    Update st_index,php, st_insert,st_delete,st_update
    yeni element elave olunmasini,silinmesini, adini deyismesini isledim

--- struktur2/st_index.php
+++ struktur2/st_index.php
@@ -48,7 +48,7 @@ function createArray($arrC){
         $arrCh['st_type'] = $arrCh[3];
         $arrCh['year'] = $arrCh[4];
 //        $arrCh['children'] = $arrCh[5];
-        $arrCh['expandedcr'] = true;
+        $arrCh['expanded'] = true;
         $arrCh['folder'] = true;
         if(count($arrCh[5])>0){
 
@@ -113,7 +113,7 @@ function unflattenArray($flatArray){
             $arrC['pId'] = $arrC[2];
             $arrC['st_type'] = $arrC[3];
             $arrC['year'] = $arrC[4];
-            $arrC['expanded2'] = true;
+            $arrC['expanded'] = true;
             $arrC['folder'] = true;
 //            $arrC['children'] = $arrC[5];
             $arrChi=array();
@@ -263,132 +263,13 @@ function unflattenArray($flatArray){
             var st_type=$('#st_type').val();
             var stTypeArray=st_type.split(",");
 
-            // var sub_array=$('#sub_array').val();
-            // var subArray=sub_array.split(",");
-
-            var childrenArray=[];
             var pushArray=[];
-
-
-            // for(var j=0;j<userArray.length;j++){
-            //     if(parentArray[j]==''){
-            //         open=true;
-            //     }else{
-            //         open=false;
-            //     }
-            //     findChild(j,userIdArray[j])
-            //
-            // }
-            // function findChild(j,parentId){
-
-            // $.ajax({
-            //     url: 'st_select.php',
-            //     type: "POST",
-            //     data: {id:1},
-            //     success: function (data) {
-            //         console.log('data ss=',data)
-
-                    var jsonARR=subArray;
-
-
-                    var ID;
-                    // $.each(jsonARR, function(k,v){
-                    //     // if(v[2]!=null){
-                    //
-                    //         ID=v[0];
-                    //         $.each(jsonARR, function(key,val){
-                    //             console.log('val=',val)
-                    //             console.log('val[2]='+val[2]+' ID='+ID)
-                    //             childrenArray=[];
-                    //             console.log('childrenArray0=',childrenArray)
-                    //             if(val[2]==ID ){///&& v[0]!=val[0]
-                    //                 // ID=val[0];
-                    //                 // childrenArray=[];
-                    //                 console.log('childrenArray1=',childrenArray)
-                    //                 childrenArray.push({"id":val[0],"title":val[1],"pId":val[2],"st_type":val[4],"year":val[3], "expanded":true,"folder": true,"children":childrenArray});
-                    //                 console.log('childrenArray2=',childrenArray)
-                    //                 zNodeArray.push(childrenArray)
-                    //             }
-                    //             // if(v[0]!=val[0]){
-                    //             //     zNodeArray.push({"id":v[0],"title":v[1],"pId":v[2],"st_type":v[4],"year":v[3], "expanded":true,"folder": true,"children":childrenArray});
-                    //             //
-                    //             // }
-                    //
-                    //         })
-                    //     // if(childrenArray!=[]){
-                    //     //     zNodeArray.push(childrenArray)
-                    //     // }
-                    //     // console.log('childrenArray=',childrenArray)
-                    //        // zNodeArray.push({"id":v[0],"title":v[1],"pId":v[2],"st_type":v[4],"year":v[3], "expanded":true,"folder": true,"children":''});
-                    //
-                    //         // if(v[2]!=null){
-                    //         //     childrenArray.push({"id":v[0],"title":v[1],"pId":v[2],"st_type":v[4],"year":v[3],"type":v[4],"author":v[2],"qty":332,"price":224});
-                    //         //     // console.log('childrenArray=',childrenArray)
-                    //         // }else{
-                    //         //     zNodeArray.push({"id":v[0],"title":v[1],"pId":v[2],"st_type":v[4],"year":v[3], "expanded":true,"folder": true,"children":childrenArray});
-                    //         //
-                    //         // }
-                    //
-                    //         console.log('zNodeArray=',zNodeArray)
-                    //        // }
-                    //
-                    // });
-
-                    // console.log('childrenArray=',childrenArray)
-                    // //zNodeArray+=[{"title":userArray[j], "expanded":true,"folder": true,"children":childrenArray}]
-                    //
-                    // // console.log('zNodeArray=',zNodeArray)
-                    // pushOldu(zNodeArray);
-            //     },
-            // });
-
-            // }
 
 
             // function pushOldu(zNodeArray) {
               pushArray = subArray;
            console.log('pushArray=', pushArray)
 
-
-            var SOURCE = [
-                {"title": "Books", "expanded": true, "folder": true, "children": [
-                        {"title": "Art of War", "type": "book", "author": "Sun Tzu", "year": -500, "qty": 21, "price": 5.95},
-                        {"title": "The Hobbit", "type": "book", "author": "J.R.R. Tolkien", "year": 1937, "qty": 32, "price": 8.97},
-                        {"title": "The Little Prince", "type": "book", "author": "Antoine de Saint-Exupery", "year": 1943, "qty": 2946, "price": 6.82},
-                        {"title": "Don Quixote", "type": "book", "author": "Miguel de Cervantes", "year": 1615, "qty": 932, "price": 15.99}
-                    ]},
-                {"title": "Music", "folder": true, "children": [
-                        {"title": "Nevermind", "type": "music", "author": "Nirvana", "year": 1991, "qty": 916, "price": 15.95},
-                        {"title": "Autobahn", "type": "music", "author": "Kraftwerk", "year": 1974, "qty": 2261, "price": 23.98},
-                        {"title": "Kind of Blue", "type": "music", "author": "Miles Davis", "year": 1959, "qty": 9735, "price": 21.90},
-                        {"title": "Back in Black", "type": "music", "author": "AC/DC", "year": 1980, "qty": 3895, "price": 17.99},
-                        {"title": "The Dark Side of the Moon", "type": "music", "author": "Pink Floyd", "year": 1973, "qty": 263, "price": 17.99},
-                        {"title": "Sgt. Pepper's Lonely Hearts Club Band", "type": "music", "author": "The Beatles", "year": 1967, "qty": 521, "price": 13.98}
-                    ]},
-                {"title": "Electronics & Computers", "expanded": true, "folder": true, "children": [
-                        {"title": "Cell Phones", "folder": true, "children": [
-                                {"title": "Moto G", "type": "phone", "author": "Motorola", "year": 2014, "qty": 332, "price": 224.99},
-                                {"title": "Galaxy S8", "type": "phone", "author": "Samsung", "year": 2016, "qty": 952, "price": 509.99},
-                                {"title": "iPhone SE", "type": "phone", "author": "Apple", "year": 2016, "qty": 444, "price": 282.75},
-                                {"title": "G6", "type": "phone", "author": "LG", "year": 2017, "qty": 951, "price": 309.99},
-                                {"title": "Lumia", "type": "phone", "author": "Microsoft", "year": 2014, "qty": 32, "price": 205.95},
-                                {"title": "Xperia", "type": "phone", "author": "Sony", "year": 2014, "qty": 77, "price": 195.95},
-                                {"title": "3210", "type": "phone", "author": "Nokia", "year": 1999, "qty": 3, "price": 85.99}
-                            ]},
-                        {"title": "Computers", "folder": true, "children": [
-                                {"title": "ThinkPad", "type": "computer", "author": "IBM", "year": 1992, "qty": 16, "price": 749.90},
-                                {"title": "C64", "type": "computer", "author": "Commodore", "year": 1982, "qty": 83, "price": 595.00},
-                                {"title": "MacBook Pro", "type": "computer", "author": "Apple", "year": 2006, "qty": 482, "price": 1949.95},
-                                {"title": "Sinclair ZX Spectrum", "type": "computer", "author": "Sinclair Research", "year": 1982, "qty": 1, "price": 529},
-                                {"title": "Apple II", "type": "computer", "author": "Apple", "year": 1977, "qty": 17, "price": 1298},
-                                {"title": "PC AT", "type": "computer", "author": "IBM", "year": 1984, "qty": 3, "price": 1235.00}
-                            ]}
-                    ]},
-                {"title": "More...", "folder": true, "lazy": true}
-            ];
-            console.log('SOURCE=',SOURCE)
-
-            // }
             $("#tree")
                 .fancytree({
                     checkbox: true,
@@ -421,6 +302,25 @@ function unflattenArray($flatArray){
                         triggerStart: ["f2", "shift+click", "mac+enter"],
                         close: function(event, data) {
                             if (data.save && data.isNew) {
+                                console.log('editttttttttttttt',data)
+                                console.log('data.cmd==',data.cmd)
+                                var PID;
+                                var title;
+                                PID=data.node.parent.data.id;
+                                title=data.node.title;
+                                console.log('PID=='+PID);
+                                console.log('title=='+title);
+
+                                $.ajax({
+                                    url: 'st_insert.php',
+                                    type: "POST",
+                                    data: { pId:PID, name:title},
+                                    success: function (data) {
+                                        console.log('data=' + data)
+                                        // members=$.parseJSON(data)
+
+                                    },
+                                });
                                 // Quick-enter: add new nodes until we hit [enter] on an empty title
                                 $("#tree").trigger("nodeCommand", {
                                     cmd: "addSibling",
@@ -494,8 +394,51 @@ function unflattenArray($flatArray){
                         // ...
                     },
                     modifyChild: function(event, data) {
-                        console.log('modifyChild')
+                        console.log('modifyChild event.type='+event.type)
+                        console.log('modifyChild data=',data)
                         data.tree.info(event.type, data);
+                        if(data.operation=='rename'){
+                            console.log('rename',data)
+                            console.log('data.cmd==',data.cmd)
+                            var ID;
+                            var title;
+                            ID=data.childNode.data.id;
+                            title=data.childNode.title;
+                            console.log('ID=='+ID);
+                            console.log('title=='+title);
+
+                            $.ajax({
+                                url: 'st_update.php',
+                                type: "POST",
+                                data: { id:ID, name:title,change:'category'},
+                                success: function (data) {
+                                    console.log('data=' + data)
+                                    // members=$.parseJSON(data)
+
+                                },
+                            });
+                        }
+                        if(data.operation=='remove'){
+                            console.log('rename',data)
+                            console.log('data.cmd==',data.cmd)
+                            var ID;
+                            var title;
+                            ID=data.childNode.data.id;
+                            // title=data.childNode.title;
+                            console.log('ID=='+ID);
+                            // console.log('title=='+title);
+
+                            $.ajax({
+                                url: 'st_delete.php',
+                                type: "POST",
+                                data: { id:ID},
+                                success: function (data) {
+                                    console.log('data=' + data)
+                                    // members=$.parseJSON(data)
+
+                                },
+                            });
+                        }
                     },
                 })
                 .on("nodeCommand", function(event, data) {
@@ -505,6 +448,7 @@ function unflattenArray($flatArray){
                         moveMode,
                         tree = $.ui.fancytree.getTree(this),
                         node = tree.getActiveNode();
+                    var say=0;
 
                     switch (data.cmd) {
 
@@ -551,7 +495,7 @@ function unflattenArray($flatArray){
                 .on("keydown", function(e) {
                     var cmd = null;
 
-                    // console.log(e.type, $.ui.fancytree.eventToString(e));
+                     console.log("keyDown"+$.ui.fancytree.eventToString(e));
                     switch ($.ui.fancytree.eventToString(e)) {
                         case "ctrl+shift+n":
                         case "meta+shift+n": // mac: cmd+shift+n
@@ -597,6 +541,7 @@ function unflattenArray($flatArray){
                             cmd = "outdent";
                     }
                     if (cmd) {
+                        console.log('trigger')
                         $(this).trigger("nodeCommand", { cmd: cmd });
                         return false;
                     }
@@ -616,6 +561,7 @@ function unflattenArray($flatArray){
              * Context menu (https://github.com/mar10/jquery-ui-contextmenu)
              */
             $("#tree").contextmenu({
+
 
                 delegate: "span.fancytree-node",
                 menu: [
@@ -659,6 +605,7 @@ function unflattenArray($flatArray){
                     },
                 ],
                 beforeOpen: function(event, ui) {
+                    console.log('beforeOpen')
                     var node = $.ui.fancytree.getNode(ui.target);
                     $("#tree").contextmenu(
                         "enableEntry",
---
+++ struktur2/st_update.php
@@ -0,0 +1,61 @@
+<?php
+
+
+include('../session.php') ;
+
+//Create variables
+
+$id                 =$_POST['id'];
+$change             = $_POST['change'];
+if(isset($_POST['name'])){
+    $name           = $_POST['name'];
+}
+if(isset($_POST['icon'])){
+    $name           = $_POST['icon'];
+}
+
+
+$sql = "UPDATE  $tbl_employee_category SET  
+		$change  = '$name'
+		WHERE id 	= '$id' ";
+
+if(mysqli_query($db, $sql) ) {
+//    echo "success";
+}
+else  {
+    echo "error" .mysqli_error($db);
+}
+
+
+//Close connection
+//mysqli_close($db);
+
+$users= "select * from $tbl_employee_category";
+$result_users = $db->query($users);
+$user=[];
+$parent=[];
+$user_id=[];
+$zNodeArray=[];
+if($result_users){
+    if ($result_users->num_rows > 0) {
+        while($row_users = $result_users->fetch_assoc()) {
+//            $zNodeArray["user"]=$row_users['user'];
+//            $zNodeArray.array_push({"id":userIdArray[j], "pId":parentArray[j],"name":userArray[j],"open":open});
+//            if($row_users['parent']==null){
+//                $open=true;
+//            }else {
+//                $open=false;
+//            }
+                array_push($zNodeArray, ['id'=>$row_users['id'],'pId'=>$row_users['parent'],'name'=>$row_users['category'],'st_type'=>$row_users['st_type'],'year'=>$row_users['create_date']]);
+
+
+
+
+
+        }
+
+    }
+}
+echo  json_encode($zNodeArray);
+
+?>
