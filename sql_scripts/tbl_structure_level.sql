9c3d0e2b2f5ae9e6cf8cbb7780a7192decc8a54d
Mayya-S <65227777+Mayya-S@users.noreply.github.com>
2020-07-20 20:29:08 +0400

    Update .sql

---
+++ sql_scripts/tbl_structure_level.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 20, 2020 at 06:28 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rhr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_structure_level`
+--
+
+CREATE TABLE `tbl_structure_level` (
+  `id` int(11) NOT NULL,
+  `struc_id` int(11) NOT NULL,
+  `title` varchar(300) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `tbl_structure_level`
+--
+
+INSERT INTO `tbl_structure_level` (`id`, `struc_id`, `title`) VALUES
+(1, 1, 'Müəssisə'),
+(2, 2, 'Direktorluq'),
+(3, 3, 'Departament'),
+(4, 4, 'Şöbə'),
+(5, 5, 'Sahə / Bölmə');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_structure_level`
+--
+ALTER TABLE `tbl_structure_level`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_structure_level`
+--
+ALTER TABLE `tbl_structure_level`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
