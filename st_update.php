2af6d523ebaad8af0e254d9ba6f032b1d924ae31
Bahruz Qasimov <Bahruz.Qasimov@pashabank.az>
2020-07-24 14:08:23 +0400

    Create st_update.php

---
+++ st_update.php
@@ -0,0 +1,50 @@
+<?php
+
+
+include('session.php') ;
+
+//Create variables
+
+$id                 =$_POST['id'];
+if(isset($_POST['change'])){
+    $change             = $_POST['change'];
+}
+
+if(isset($_POST['name'])){
+    $name           = $_POST['name'];
+}
+if(isset($_POST['icon'])){
+    $name           = $_POST['icon'];
+}
+if(isset($_POST['createDate'])){
+    $createDate        = $_POST['createDate'];
+    $endDate           = $_POST['endDate'];
+
+    $createDate = strtr( $createDate , '/', '-');
+    $createDate= date('Y-m-d', strtotime($createDate));
+
+    $endDate = strtr( $endDate , '/', '-');
+    $endDate= date('Y-m-d', strtotime($endDate));
+
+    $sql = "UPDATE  $tbl_employee_category SET  
+			create_date  = '$createDate',
+		end_date  = '$endDate'
+		WHERE id 	= '$id'";
+}else{
+    $sql = "UPDATE  $tbl_employee_category SET  
+		$change  = '$name'
+		WHERE id 	= '$id'";
+}
+
+
+
+
+if(mysqli_query($db, $sql) ) {
+//    echo "success";
+}
+else  {
+    echo "errorwww" .mysqli_error($db);
+}
+
+include ('st_select.php');
+?>
