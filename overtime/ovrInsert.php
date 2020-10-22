4619fa6c3bf5e012f34239ad0072a34ae3174c1b
Bahruz Qasimov <Bahruz.Qasimov@pashabank.az>
2020-10-22 14:22:00 +0400

    Update ovrInsert.php

--- overtime/ovrInsert.php
+++ overtime/ovrInsert.php
@@ -1,46 +1,26 @@
 <?php 
  
 include('../session.php') ;
-
-function generateRandomString($length = 2) {
-    return substr(str_shuffle(str_repeat($x='ABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
-}
-
+ 
   //Create variables
 
-	$schname_name=$_POST['schname_name'];
-	$sch_start_date_name=$_POST['sch_start_date_name'];
-    $sch_expire_date_name = $_POST['sch_expire_date_name'];
-	$tm_type_name = $_POST['tm_type_name'];
-	$sch_type_name = $_POST['sch_type_name'];
-	$reduce_type_name = $_POST['reduce_type_name'];
-	$red_working_hours_name = $_POST['red_working_hours_name'];
-	$reduce_reason_name = $_POST['reduce_reason_name'];
-	$start_time_name = $_POST['start_time_name'];
-	$end_time_name = $_POST['end_time_name'];
-	$break_start_time_name = $_POST['break_start_time_name'];
-	$break_end_time_name = $_POST['break_end_time_name'];
-	$dinner_start_time_name = $_POST['dinner_start_time_name'];
-	$dinner_end_time_name = $_POST['dinner_end_time_name'];
-	$night_time_name = $_POST['night_time_name'];
-	$company_id_name = $_POST['company_id_name']; 
-	
-	/**schcode  generate*/
-	$schcode =generateRandomString();
-	$schcode =$schcode.mt_rand(1000,9999);
- 
+	$employee_id_name=$_POST['employee_id_name'];
+	$ovr_start_date_name=$_POST['ovr_start_date_name'];
+    $ovr_end_date_name = $_POST['ovr_end_date_name'];
+	$calc_status_name = $_POST['calc_status_name'];
+	$overtime_period_name = $_POST['overtime_period_name'];
+	$overtime_status_name = $_POST['overtime_status_name'];
+$ovr_start_date_name = strtr($ovr_start_date_name, '/', '-');
+$ovr_start_date_name= date('Y-m-d', strtotime($ovr_start_date_name));
 
-$sch_start_date_name = strtr($sch_start_date_name, '/', '-');
-$sch_start_date_name= date('Y-m-d', strtotime($sch_start_date_name));
-
-$sch_expire_date_name = strtr($sch_expire_date_name, '/', '-');
-$sch_expire_date_name= date('Y-m-d', strtotime($sch_expire_date_name));
+$ovr_end_date_name = strtr($ovr_end_date_name, '/', '-');
+$ovr_end_date_name= date('Y-m-d', strtotime($ovr_end_date_name));
 
  
 //-$passport_date=date('Y-m-d',strtotime($passport_date));
-$sql = "INSERT INTO $tbl_schedules (
-		  id, 		 sch_name,   sch_code,         company_id,             start_date, 			   expire_date,         tm_type,         sch_type,         reduce_type,         red_working_hours,         reduce_reason,         start_time,         end_time,         break_start_time,         break_end_time,         dinner_start_time,           dinner_end_time,          night_time,  insert_user ) 
-VALUES (NULL, '$schname_name', '$schcode', '$company_id_name', '$sch_start_date_name', '$sch_expire_date_name', '$tm_type_name', '$sch_type_name', '$reduce_type_name', '$red_working_hours_name', '$reduce_reason_name', '$start_time_name', '$end_time_name', '$break_start_time_name', '$break_end_time_name', '$dinner_start_time_name', 	'$dinner_end_time_name', '$night_time_name',       '$uid') ";
+$sql = "INSERT INTO $tbl_employee_overtimes (
+		  id ,	emp_id ,		start_date ,			expire_date, 			calc_status ,		overtime_status, 		overtime_period, 		insert_user ) 
+VALUES (NULL, '$employee_id_name', '$ovr_start_date_name', '$ovr_end_date_name', '$calc_status_name', '$overtime_status_name', '$overtime_period_name','$uid') ";
   
 
   if(!mysqli_query($db, $sql)) {
