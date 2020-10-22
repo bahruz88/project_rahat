2b3ed998abd045f0f88b0b88ccaaf90e9f3d0fd9
Bahruz Qasimov <Bahruz.Qasimov@pashabank.az>
2020-10-22 17:06:04 +0400

    Update get_ovr.php

--- overtime/get_ovr.php
+++ overtime/get_ovr.php
@@ -5,12 +5,12 @@ $sql_ovr = "
 SELECT eo.id,com.company_name, DATE_FORMAT(eo.start_date,'%d/%m/%Y') start_date,
 DATE_FORMAT(eo.expire_date,'%d/%m/%Y') expire_date, concat(emp.lastname,' ', emp.firstname ,' ', emp.surname) full_name ,
 yn.chois_desc,p.period_desc,ocs.status_desc
-FROM tbl_employee_overtimes eo inner join   
-tbl_employees emp on eo.emp_id=emp.id inner join 
-tbl_overtime_calc_status ocs  on eo.calc_status=ocs.status_id and  ocs.lang='$site_lang' inner join 
-tbl_yesno  yn  on eo.overtime_status=yn.chois_id and  yn.lang='$site_lang'  inner join
-tbl_periods p  on  eo.overtime_period=p.period_id and  p.lang='$site_lang' inner  join  
-tbl_companies com  on emp.company_id=com.id 
+FROM $tbl_employee_overtimes eo inner join   
+$tbl_employees emp on eo.emp_id=emp.id inner join 
+$tbl_overtime_calc_status ocs  on eo.calc_status=ocs.status_id and  ocs.lang='$site_lang' inner join 
+$tbl_yesno  yn  on eo.overtime_status=yn.chois_id and  yn.lang='$site_lang'  inner join
+$tbl_periods p  on  eo.overtime_period=p.period_id and  p.lang='$site_lang' inner  join  
+$tbl_companies com  on emp.company_id=com.id 
  where eo.status=1 ";
 
 					$result_ovr  = $db->query($sql_ovr);
