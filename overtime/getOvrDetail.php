7406722371dbf95b24e4515b4302efa36a61a82a
Bahruz Qasimov <Bahruz.Qasimov@pashabank.az>
2020-10-26 02:01:41 +0400

    Update getOvrDetail.php

--- overtime/getOvrDetail.php
+++ overtime/getOvrDetail.php
@@ -3,21 +3,22 @@
  
  $ovrid = $_POST['ovrid'];
  
-$sql_sch = "SELECT DATE_FORMAT(eo.start_date,'%d/%m/%Y') start_date,
-DATE_FORMAT(eo.expire_date,'%d/%m/%Y') expire_date, concat(emp.lastname,' ', emp.firstname ,' ', emp.surname) full_name ,
-yn.chois_desc,p.period_desc,ocs.status_desc
-FROM tbl_employee_overtimes eo inner join   
-tbl_employees emp on eo.emp_id=emp.id inner join 
-tbl_overtime_calc_status ocs  on eo.calc_status=ocs.status_id and  ocs.lang='az' inner join 
-tbl_yesno  yn  on eo.overtime_status=yn.chois_id and  yn.lang='az'  inner join
-tbl_periods p  on  eo.overtime_period=p.period_id and  p.lang='az'
-  eo.id='$ovrid'";
-  $result_sch= $db->query($sql_sch);
+$sql_ovr = "SELECT  com.id compid,eo.start_date , eo.expire_date, emp.id empid,
+yn.chois_id,p.period_id,ocs.status_id
+FROM 
+$tbl_employee_overtimes eo inner join   
+$tbl_employees emp on eo.emp_id=emp.id inner join 
+$tbl_overtime_calc_status ocs  on eo.calc_status=ocs.status_id and ocs.lang='az' inner join 
+$tbl_yesno  yn  on  eo.overtime_status=yn.chois_id and  yn.lang='az' inner join
+$tbl_periods p  on  eo.overtime_period=p.period_id and  p.lang='az'  inner join
+$tbl_companies com  on emp.company_id=com.id 
+  where eo.id='$ovrid'";
+  $result_ovr= $db->query($sql_ovr);
  $data = array();
-if ($result_sch->num_rows > 0) 
+if ($result_ovr->num_rows > 0) 
 {
- $row_sch = $result_sch->fetch_assoc();
- $data = $row_sch;
+ $row_ovr = $result_ovr->fetch_assoc();
+ $data = $row_ovr;
 }
 echo json_encode($data);
 ?>
