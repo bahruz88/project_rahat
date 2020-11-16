7c73da69bf0c175c202414a37e8a7442a322339f
Bahruz Qasimov <Bahruz.Qasimov@pashabank.az>
2020-11-17 02:26:24 +0400

    Update ovrUpdate.php

--- overtime/ovrUpdate.php
+++ overtime/ovrUpdate.php
@@ -3,32 +3,19 @@
 include('../session.php') ;
 
   //Create variables
-    $schid=$_POST['update_schid_name'];
-	$schname_name=$_POST['update_schname_name'];
-	$sch_start_date_name=$_POST['update_sch_start_date_name'];
-    $sch_expire_date_name = $_POST['update_sch_expire_date_name'];
-	$tm_type_name = $_POST['update_tm_type_name'];
-	$sch_type_name = $_POST['update_sch_type_name'];
-	$reduce_type_name = $_POST['update_reduce_type_name'];
-	$red_working_hours_name = $_POST['update_red_working_hours_name'];
-	$reduce_reason_name = $_POST['update_reduce_reason_name'];
-	$start_time_name = $_POST['update_start_time_name'];
-	$end_time_name = $_POST['update_end_time_name'];
-	$break_start_time_name = $_POST['update_break_start_time_name'];
-	$break_end_time_name = $_POST['update_break_end_time_name'];
-	$dinner_start_time_name = $_POST['update_dinner_start_time_name'];
-	$dinner_end_time_name = $_POST['update_dinner_end_time_name'];
-	$night_time_name = $_POST['update_night_time_name'];
-	$company_id_name = $_POST['update_company_id_name']; 
+    $ovrid=$_POST['update_ovrid_name'];
+	$update_employee_id_name=$_POST['update_employee_id_name'];
+	$update_ovr_start_date_name=$_POST['update_ovr_start_date_name'];
+    $update_ovr_end_date_name = $_POST['update_ovr_end_date_name'];
+	$update_calc_status_name = $_POST['update_calc_status_name'];
+	$update_overtime_period_name = $_POST['update_overtime_period_name'];
+	$update_overtime_status_name = $_POST['update_overtime_status_name'];
+	$update_ovr_start_date_name = strtr($update_ovr_start_date_name, '/', '-');
+	$update_ovr_start_date_name= date('Y-m-d', strtotime($update_ovr_start_date_name));
+	$update_ovr_end_date_name = strtr($update_ovr_end_date_name, '/', '-');
+	$update_ovr_end_date_name= date('Y-m-d', strtotime($update_ovr_end_date_name));
 	
-  
-
-$sch_start_date_name = strtr($sch_start_date_name, '/', '-');
-$sch_start_date_name= date('Y-m-d', strtotime($sch_start_date_name));
-
-$sch_expire_date_name = strtr($sch_expire_date_name, '/', '-');
-$sch_expire_date_name= date('Y-m-d', strtotime($sch_expire_date_name));
-
+ 
 
 
 
@@ -36,26 +23,16 @@ $sch_expire_date_name= date('Y-m-d', strtotime($sch_expire_date_name));
 //-$passport_date=date('Y-m-d',strtotime($passport_date));
 
 $sql="
-update  $tbl_schedules SET 
+update  $tbl_employee_overtimes SET 
 
-sch_name='$schname_name' ,
-company_id='$company_id_name',
-start_date='$sch_start_date_name' ,
-expire_date='$sch_expire_date_name',
-tm_type='$tm_type_name' ,
-sch_type='$sch_type_name' ,
-reduce_type='$reduce_type_name',
-red_working_hours= '$red_working_hours_name',
-reduce_reason='$reduce_reason_name',
-start_time='$start_time_name',
-end_time='$end_time_name',
-break_start_time='$break_start_time_name',
-break_end_time='$break_end_time_name',
-dinner_start_time='$dinner_start_time_name',
-dinner_end_time='$dinner_end_time_name',
-night_time='$night_time_name',
+emp_id='$update_employee_id_name' ,
+start_date='$update_ovr_start_date_name' ,
+expire_date='$update_ovr_end_date_name',
+calc_status='$update_calc_status_name' ,
+overtime_status='$update_overtime_status_name' ,
+overtime_period='$update_overtime_period_name',
 update_user= '$uid'
-where id='$schid' "; 
+where id='$ovrid' "; 
 
   if(!mysqli_query($db, $sql)) {
         echo "Error" .mysqli_error($db);
