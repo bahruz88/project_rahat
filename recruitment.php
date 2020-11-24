d458a06cce3c2459a04ab7450c90f903fe75db6c
Bahruz Qasimov <Bahruz.Qasimov@pashabank.az>
2020-11-25 01:40:50 +0400

    Update recruitment.php

--- recruitment.php
+++ recruitment.php
@@ -10,7 +10,7 @@ $site_lang=$_SESSION['dil'] ;
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   
-  <title>RahatHR</title>
+  <title><?php echo $company_name ; ?></title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" >
