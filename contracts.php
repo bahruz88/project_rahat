0e176d12a8f5c3fe595aaf934478e615eb0a43de
Bahruz Qasimov <Bahruz.Qasimov@pashabank.az>
2020-11-25 02:29:08 +0400

    Update contracts.php

--- contracts.php
+++ contracts.php
@@ -65,7 +65,7 @@ $result_commands = $db->query($commands);
         <a href="index3.html" class="brand-link">
             <img src="dist/img/rhr.png" alt="RahatHR Logo" class="brand-image img-circle elevation-3"
                  style="opacity: .8">
-            <span class="brand-text font-weight-light">RahatHR</span>
+            <span class="brand-text font-weight-light"><?php echo $company_name ; ?></span>
         </a>
 
         <!-- Sidebar -->
