8d7ebb12b657666dbb80bfe609047ed84cce3d48
Bahruz Qasimov <Bahruz.Qasimov@pashabank.az>
2020-11-25 01:41:21 +0400

    Update general.php

--- general.php
+++ general.php
@@ -40,7 +40,7 @@ if($result_users){
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>RahatHR</title>
+    <title><?php echo $company_name ; ?></title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
