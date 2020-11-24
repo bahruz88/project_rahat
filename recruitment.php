bc28c940e0cbe09cc4ab1ccb6194a423407a9492
Bahruz Qasimov <Bahruz.Qasimov@pashabank.az>
2020-11-25 02:29:35 +0400

    Update recruitment.php

--- recruitment.php
+++ recruitment.php
@@ -81,7 +81,7 @@ $site_lang=$_SESSION['dil'] ;
     <a href="index3.html" class="brand-link">
       <img src="dist/img/rhr.png" alt="RahatHR Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">RahatHR</span>
+      <span class="brand-text font-weight-light"><?php echo $company_name ; ?></span>
     </a>
 
     <!-- Sidebar -->
